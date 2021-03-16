67d2cce788383af1a230c3f894f530cc644e71b4
Dini68 <kdini68@gmail.com>
2021-03-16 17:32:49 +0100

    DigitsCounter kész, tesztelve.

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -0,0 +1,19 @@
+package hu.nive.ujratervezes.zarovizsga.digitscounter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DigitsCounter {
+    public int getCountOfDigits(String s) {
+        if (s == null || s.equals("")) {
+            return 0;
+        }
+        Set<Character> digits = new HashSet<>();
+        for (char c: s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digits.add(c);
+            }
+        }
+        return digits.size();
+    }
+}
